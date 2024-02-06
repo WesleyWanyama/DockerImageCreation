3db01ba561798139fc881741d56f056ff9d887cc
WesleyWanyama <83776940+WesleyWanyama@users.noreply.github.com>
2024-02-06 12:53:30 +0300

    fix : Error on tag

--- app/setup.sh
+++ app/setup.sh
@@ -4,7 +4,7 @@
 docker login
 
 #Tag the image
-docker tag sample-app:latest wesley3015/sample-app:v1
+docker tag sample-app:v1 wesley3015/sample-app:v1
 
 #Push the image
 docker push wesley3015/sample-app:v1
