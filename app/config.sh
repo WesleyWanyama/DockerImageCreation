122a39442ed0576e23d7ab5987c4f520cb543d68
WesleyWanyama <83776940+WesleyWanyama@users.noreply.github.com>
2024-02-26 12:29:55 +0300

    feat : add nginx folder

--- app/config.sh
+++ app/config.sh
@@ -1,6 +1,8 @@
-docker run -d --name my-nginx -p 9000:80 -p 9001:443 \
--v /path/to/http.conf:/etc/nginx/conf.d/http.conf \
--v /path/to/https.conf:/etc/nginx/conf.d/https.conf \
--v /path/to/nginx.crt:/etc/nginx/ssl/nginx.crt \
--v /path/to/nginx.key:/etc/nginx/ssl/nginx.key \
+#!/bin/bash
+
+docker run -d -p 9003:443 nginx:1.24  \
+-v http.conf:/etc/nginx/conf.d/http.conf \
+-v https.conf:/etc/nginx/conf.d/https.conf \
+-v nginx.crt:/etc/nginx/ssl/nginx.crt \
+-v nginx.key:/etc/nginx/ssl/nginx.key \
 nginx
